5bde677d82db25808d5ee7ae423f376a43211532
Viktor Lavrenenko <lavrenenkoviktoren@gmail.com>
2022-09-28 14:09:43 +0300

    Change import paths

--- contracts/DamnValuableToken.sol
+++ contracts/DamnValuableToken.sol
@@ -2,7 +2,7 @@
 
 pragma solidity 0.8.13;
 
-import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 /**
  * @title DamnValuableToken
