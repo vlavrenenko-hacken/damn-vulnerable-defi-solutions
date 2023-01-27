8d794bc3672d288e3be8eca3e8c0ec8cfaeed308
Viktor Lavrenenko <lavrenenkoviktoren@gmail.com>
2023-01-27 13:15:24 +0200

    Add new configuration

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,40 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@openzeppelin/hardhat-upgrades";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.13",
+  solidity: {
+    compilers: [
+    {
+      version: "0.8.13",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200
+        }
+      }
+    },
+    {
+      version: "0.6.6",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200
+        }
+      }
+    },
+    {
+      version: "0.5.16",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200
+        }
+      }
+    }
+
+    ]
+  }
 };
 
 export default config;
